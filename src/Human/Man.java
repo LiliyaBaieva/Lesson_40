364244edb7c8aedb8685c14038d9ca7efbb49c64
Liliia Baieva <555blv@gmail.com>
2023-03-15 12:09:23 +0100

    add printHuman() in Man

--- src/Human/Man.java
+++ src/Human/Man.java
@@ -1,5 +1,11 @@
 package Human;
 
 public class Man extends Human {
-
+  public Man(String name){
+    super(name);
+  }
+  @Override
+  public void printHuman(String name){
+    super.printHuman("Мужчина по имени " + name);
+  }
 }
