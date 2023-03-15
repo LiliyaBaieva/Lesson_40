7c94e0ce2e33d327794c6407221a6d82b9488d47
Liliia Baieva <555blv@gmail.com>
2023-03-15 12:26:08 +0100

    Add tests

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import Human.Human;
+import Human.Man;
+import Human.Woman;
+
 public class Main {
   public static void main(String[] args) {
 // Создайте класс "Человек", конструктор которого принимает имя.
@@ -10,5 +14,21 @@ public class Main {
 // Попытка вывести их на экран должна приводить к выводу строки
 // "Мужчина по имени ..." или "Женщина по имени ..."
 // Метод "приветствие" класса "Женщина" должен выводить строку: "Я родилась!"
+    Human human = new Human("Барабашка");
+    Man man = new Man("Василий");
+    Woman woman = new Woman("Маруся");
+
+    human.printHuman(human.getName());
+    human.greetings();
+    System.out.println();
+
+    man.printHuman(man.getName());
+    man.greetings();
+    System.out.println();
+
+    woman.printHuman(woman.getName());
+    woman.greetings();
+    System.out.println();
+
   }
 }
