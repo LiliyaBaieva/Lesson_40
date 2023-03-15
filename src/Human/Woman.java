a78a5a0caae0151e5348a2ea23644dee18b229ae
Liliia Baieva <555blv@gmail.com>
2023-03-15 12:12:17 +0100

    add greetings() in Woman

--- src/Human/Woman.java
+++ src/Human/Woman.java
@@ -8,4 +8,9 @@ public class Woman extends Human{
   public void printHuman(String name){
     super.printHuman("Женщина по имени " + name);
   }
+
+  @Override
+  public void greetings(){
+    System.out.println("Я родилась!");
+  }
 }
